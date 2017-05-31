48d10debfea056c96385301fab0384e658197feb
Sahil Patel <sahilmp8@gmail.com>
2017-05-31 20:25:31 +1000

    ignore csv files and neuralNetwork update

--- R/neuralNetwork.R
+++ R/neuralNetwork.R
@@ -26,6 +26,8 @@ neuralNetwork <- setRefClass("neuralNetwork",
                                      
                                    )
                                  
+                                 return(invisible(NULL))
+                                 
                                },
                                # sigmoid function to supress weights
                                activation_function = function(x){
@@ -53,14 +55,37 @@ neuralNetwork <- setRefClass("neuralNetwork",
                                  # output errors (target-actual)
                                  output_errors <- targets - final_outputs
                                  
-                                 print(final_outputs)
-                                 
-                                 print(output_errors)
                                  
                                  hidden_errors <- crossprod(t(.self$weightsoutput), output_errors)
                                  
-                                 .selfweightsoutput <- .selfweightsoutput + (learningrate *(crossprod((output_errors*final_inputs*(1-final_outputs)),  ))) 
-                                 print(hidden_errors)
+                                 # output layer weights update
+                                
+                                 
+                                 # output weights update (3rd layer)
+                                 .self$weightsoutput <- .self$weightsoutput + learningrate*( tcrossprod( x = (output_errors*final_outputs*(1-final_outputs)),
+                                                                                                         y = hidden_outputs)  )
+                                 
+                                 # hidden weights update (2nd layer)
+                                 .self$weightshidden <- .self$weightshidden + learningrate*( tcrossprod( x = (hidden_errors*hidden_outputs*(1-hidden_outputs)),
+                                                                                                         y = inputs)  )
+                                 
+                                 return(invisible(NULL))
+                               },
+                               query = function(inputs_list){
+                                 
+                                 # conver to matrix
+                                 inputs = matrix(inputs_list)
+                                 
+                                 # input layer
+                                 hidden_inputs = crossprod(.self$weightshidden, inputs)
+                                 hidden_outputs = .self$activation_function(hidden_inputs)
+                                 
+                                 # final layer
+                                 final_inputs = crossprod(.self$weightsoutput, hidden_outputs)
+                                 final_outputs = .self$activation_function(hidden_inputs)
+                                 
+                                 
+                                 return(final_outputs)
                                  
                                }
                             )
@@ -69,8 +94,53 @@ neuralNetwork <- setRefClass("neuralNetwork",
   
 )
 
-n <- neuralNetwork(inputnodes = 3, hiddennodes = 3, outputnodes = 3)
+displayDigit <- function(X){
+  m <- matrix(unlist(X),nrow = 28,byrow = T)
+  m <- t(apply(m, 2, rev))
+  image(m,col=grey.colors(255))
+}
+
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+
+input_nodes = 784
+hidden_nodes = 100
+output_nodes = 10
+learning_rate = 0.3
+
+n <- neuralNetwork(inputnodes = input_nodes, hiddennodes = hidden_nodes, outputnodes = output_nodes, learningrate = learning_rate)
 n$initalize()
 
-n$train(c(1,2,3), c(1,2,3))
+library(data.table)
+
+train <- fread('mnist_train.csv')
+test <- fread('mnist_test.csv')
+
+for(row in 1:nrow(train)) {
+  
+  observation <- as.numeric(train[row])
+  
+  inputs <- ((observation/255.0)*0.99)+0.01
+  
+  targets <- matrix(0L, nrow = output_nodes)
+  
+  targets[observation[1]] <- 0.99
+  
+  tr <<- targets
+  
+  ip <<- inputs
+  n$train(inputs, targets)
+  
+  
+  print(row)
+  
+}
+
+train.c <- (train[1,-1]/255.0*0.99)+0.01
+
+
+#image(matrix(unlist(train[1][,-1]), ncol=28, nrow=28))
+
+
+
 
